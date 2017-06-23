f9ab9774e75f91b7ee22e075ae019f9e83b1e9dd
danya07 <danyapin07@gmail.com>
2017-06-23 00:26:45 -0500

    initial import

---
+++ src/main/java/com/tenoch/midulceengie/MiDulceEngieApplication.java
@@ -0,0 +1,12 @@
+package com.tenoch.midulceengie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiDulceEngieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiDulceEngieApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tenoch/midulceengie/MiDulceEngieApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tenoch.midulceengie;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MiDulceEngieApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
