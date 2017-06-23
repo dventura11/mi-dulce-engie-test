c6bb2ed8ec7cc1ceeebafd99215cc733d7985a4b
danya07 <danyapin07@gmail.com>
2017-06-23 10:41:53 -0500

    Index Probé que entrará a la página. Probé que existiera el video. Probé que diera Play. Probé que se reproduzca el video. Probé que la URL estuviera correcta

--- src/test/java/com/tenoch/midulceengie/IndexPageTest.java
+++ src/test/java/com/tenoch/midulceengie/IndexPageTest.java
@@ -1,6 +1,11 @@
 package com.tenoch.midulceengie;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import junit.framework.TestCase;
@@ -11,8 +16,10 @@ public class IndexPageTest extends TestCase {
 
 	@Override
 	protected void setUp() throws Exception {
-	
+		System.setProperty ("webdriver.chrome.driver", "/home/danya07/Documents/chromedriver");
+		//browser = new ChromeDriver();
 		browser = new HtmlUnitDriver();
+
 		browser.get("http://localhost:8085");
 		
 	}
@@ -27,4 +34,14 @@ public class IndexPageTest extends TestCase {
 		String actual = browser.getTitle();
 		assertEquals(esperado, actual);
 	}
+	public void testVideo(){
+		WebElement videoFrame = browser.findElement(By.xpath("//*[@id=\"video\"]/p/iframe"));
+		assertNotNull(videoFrame);
+		videoFrame.click();
+		String urlActual = videoFrame.getAttribute("src");
+		assertNotNull(urlActual);
+		String esperado = "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fmydulceengie%2Fvideos%2F881827315271578%2F&show_text=0&width=560";
+		assertEquals(esperado, urlActual);
+	}
+	
 }
